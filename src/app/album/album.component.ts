241b4eb9e103400f2be942d6deb3cb118519af45
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-01-10 13:23:46 +0100

    added to add

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from "../service/spotify.service";
+import { Album } from "../model/album.model";
+import { Artist } from "../model/artist.model";
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-album',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+    id: string;
+  artist: Artist[];
+  album: Album[];
 
-  constructor() { }
+  constructor(private _spotifyService: SpotifyService, private _route: ActivatedRoute) { }
+
 
   ngOnInit() {
+     this._route.params.map(params => params['id'])
+      .subscribe((id) => {
+        this._spotifyService.getAlbum(id)
+          .subscribe(album => {
+            this.album = album;
+          })
+      })
   }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { ModuleWithProviders }  from '@angular/core';
 import { SearchComponent } from "./search/search.component";
 import { AboutComponent } from './about/about.component';
 import { ArtistComponent } from "../app/artist/artist.component";
+import { AlbumComponent } from "./album/album.component";
 
 
 
 const appRoutes: Routes = [
     { path: '', component: SearchComponent },
     { path: 'about', component: AboutComponent },
-    {path:'artist/:id',component:ArtistComponent}
+    {path:'artist/:id',component:ArtistComponent},
+    {path:'',component:AlbumComponent}
 ];
 
 export const appRouterProviders = RouterModule.forRoot(appRoutes);
