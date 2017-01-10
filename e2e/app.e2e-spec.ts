e1befc2a0101480e88dcb3ec40c34558838d718c
Georgios Chatziefstratiou <gchatz@key-software.com>
2017-01-10 09:20:38 +0100

    merge finished :

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-import { SpotifyPage } from './app.po';
 
-describe('spotify App', function() {
-  let page: SpotifyPage;
-
-  beforeEach(() => {
-    page = new SpotifyPage();
-=======
 import { SpotifyAppPage } from './app.po';
 
 describe('spotify-app App', function() {
@@ -14,7 +6,7 @@ describe('spotify-app App', function() {
 
   beforeEach(() => {
     page = new SpotifyAppPage();
->>>>>>> d88f20f86f03ae1bf23e56a730134c48f0e5d513
+
   });
 
   it('should display message saying app works', () => {
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,10 +1,8 @@
 import { browser, element, by } from 'protractor';
 
-<<<<<<< HEAD
-export class SpotifyPage {
-=======
+
 export class SpotifyAppPage {
->>>>>>> d88f20f86f03ae1bf23e56a730134c48f0e5d513
+
   navigateTo() {
     return browser.get('/');
   }
