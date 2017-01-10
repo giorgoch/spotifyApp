446ecf5895811b71a04644a17d1db3ff965b4eb0
Georgios Chatziefstratiou <gchatz@key-software.com>
2017-01-10 12:42:50 +0100

    images

--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -16,18 +16,22 @@ export class ArtistComponent implements OnInit {
   artist: Artist[];
   albums: Album[];
 
-  constructor(private _spotifyService: SpotifyService,private _route:ActivatedRoute) { }
+  constructor(private _spotifyService: SpotifyService, private _route: ActivatedRoute) { }
 
   ngOnInit() {
-    this._route.params.map(params => params['id'] )
-    .subscribe((id) =>{
-      this._spotifyService.getArtist(id)
-      .subscribe(artist=> {
-        this.artist=artist;
+    this._route.params.map(params => params['id'])
+      .subscribe((id) => {
+        this._spotifyService.getArtist(id)
+          .subscribe(artist => {
+            this.artist = artist;
+          })
+
+
+        this._spotifyService.getAlbums(id)
+          .subscribe(albums => {
+            this.albums = albums.items;
+          })
       })
-    })
   }
-
-
 
 }
