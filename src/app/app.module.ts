c53237758abcf7e1f0faddd48f40a563293e75d5
Georgios Chatziefstratiou <gchatz@key-software.com>
2017-01-10 09:16:41 +0100

    changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { AboutComponent } from './about/about.component';
@@ -16,22 +16,13 @@ import { appRouterProviders} from "./app.routes";
     NavbarComponent,
     SearchComponent,
     AboutComponent
-=======
-
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> d88f20f86f03ae1bf23e56a730134c48f0e5d513
   ],
   imports: [
     BrowserModule,
     FormsModule,
-<<<<<<< HEAD
     HttpModule,
-    appRouterProviders
-=======
+    appRouterProviders,
     HttpModule
->>>>>>> d88f20f86f03ae1bf23e56a730134c48f0e5d513
   ],
   providers: [],
   bootstrap: [AppComponent]
